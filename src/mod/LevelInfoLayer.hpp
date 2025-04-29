1df8b371e172f6ed2e1bca248be4ef8fd4e59724
devlin <jarvisdevilyt@gmail.com>
2025-04-29 00:29:20 -0500

    aaaaa

--- src/mod/LevelInfoLayer.hpp
+++ src/mod/LevelInfoLayer.hpp
@@ -34,7 +34,7 @@ class $modify(ADBLevelInfoLayer, LevelInfoLayer) {
                         geode::queueInMainThread([body, levelID]() {
                             geode::createQuickPopup(
                                 "AbuseDB",
-                                "This level has been flagged by AbuseDB for inappropriate, misleading or harmful content.\n\nReason: " + gd::string(body),
+                                gd::string("This level has been flagged by AbuseDB for inappropriate, misleading or harmful content.\n\nReason: ") + body,
                                 "Hide", "OK",
                                 [levelID](auto, bool btn2) {
                                     if (!btn2) {
